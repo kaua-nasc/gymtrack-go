36dd239e9cd1c8f281a5caaca1005799cd531056
Kauã Alves <kauaalvesnascimento22@gmail.com>
2026-05-22 11:57:01 -0300

    feat: implement IPv4 database driver and update connection settings

--- libs/db/connection.go
+++ libs/db/connection.go
@@ -2,14 +2,37 @@ package db
 
 import (
 	"database/sql"
+	"database/sql/driver"
+	"net"
+	"time"
 
-	_ "github.com/lib/pq"
+	"github.com/lib/pq"
 )
+
+type ipv4Dialer struct{}
+
+func (d *ipv4Dialer) Dial(network, address string) (net.Conn, error) {
+	return net.Dial("tcp4", address)
+}
+
+func (d *ipv4Dialer) DialTimeout(network, address string, timeout time.Duration) (net.Conn, error) {
+	return net.DialTimeout("tcp4", address, timeout)
+}
+
+type IPv4Driver struct{}
+
+func (d *IPv4Driver) Open(name string) (driver.Conn, error) {
+	return pq.DialOpen(&ipv4Dialer{}, name)
+}
+
+func init() {
+	sql.Register("postgres-ipv4", &IPv4Driver{})
+}
 
 // NewConnection opens a new database connection pool.
 // The DSN should be in the format: "postgres://user:pass@host:port/dbname?sslmode=disable"
 func NewConnection(dsn string) (*sql.DB, error) {
-	db, err := sql.Open("postgres", dsn)
+	db, err := sql.Open("postgres-ipv4", dsn)
 	if err != nil {
 		return nil, err
 	}
@@ -19,8 +42,8 @@ func NewConnection(dsn string) (*sql.DB, error) {
 	}
 
 	// Connection Pool Settings
-	db.SetMaxOpenConns(25)
-	db.SetMaxIdleConns(25)
+	db.SetMaxOpenConns(15)
+	db.SetMaxIdleConns(15)
 	db.SetConnMaxLifetime(0) // Connections are reused forever if not closed by the DB
 
 	return db, nil
