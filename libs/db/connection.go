3120de84ce9ba7936cf9df8304a37ca1251abe07
Kauã Alves <kauaalvesnascimento22@gmail.com>
2026-05-22 12:05:55 -0300

    refactor: simplify database connection by removing IPv4 driver support

--- libs/db/connection.go
+++ libs/db/connection.go
@@ -2,37 +2,14 @@ package db
 
 import (
 	"database/sql"
-	"database/sql/driver"
-	"net"
-	"time"
 
-	"github.com/lib/pq"
+	_ "github.com/lib/pq"
 )
-
-type ipv4Dialer struct{}
-
-func (d *ipv4Dialer) Dial(network, address string) (net.Conn, error) {
-	return net.Dial("tcp4", address)
-}
-
-func (d *ipv4Dialer) DialTimeout(network, address string, timeout time.Duration) (net.Conn, error) {
-	return net.DialTimeout("tcp4", address, timeout)
-}
-
-type IPv4Driver struct{}
-
-func (d *IPv4Driver) Open(name string) (driver.Conn, error) {
-	return pq.DialOpen(&ipv4Dialer{}, name)
-}
-
-func init() {
-	sql.Register("postgres-ipv4", &IPv4Driver{})
-}
 
 // NewConnection opens a new database connection pool.
 // The DSN should be in the format: "postgres://user:pass@host:port/dbname?sslmode=disable"
 func NewConnection(dsn string) (*sql.DB, error) {
-	db, err := sql.Open("postgres-ipv4", dsn)
+	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		return nil, err
 	}
